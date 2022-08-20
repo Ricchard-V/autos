486dc410ecea1c39c654b523617f55e0e814eba2
Ricchard-V <richards_9703@hotmail.com>
2022-08-20 18:08:13 -0500

    habilitado boton log out
    se hablilita el boton de logout en la parte superior de admin.index

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,7 +1,22 @@
 @extends('layouts.app')
 @section('header')
 	<div class="py-3 px-2">
-		<div>{{ Auth::user()->name }} ({{ (Request::ip())}})</div>
+		<div>
+			<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->name }} ({{ (Request::ip())}})
+            </a>
+	        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+	            <a class="dropdown-item" href="{{ route('logout') }}"
+	               onclick="event.preventDefault();
+	                             document.getElementById('logout-form').submit();">
+	                {{ __('Logout') }}
+	            </a>
+
+	            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+	                @csrf
+	            </form>
+	        </div>
+		</div>
 	</div>
 	<hr>
 @section('main')
